476a6e26f25f1e31cb900885a2cd60ffeb0d70cc
NVARUN31 <vn6756@srmist.edu.in>
2026-09-19 22:51:29 +0530

    Add Week 4 practice problems

---
+++ Week-1/SrmStudentDemo.java
@@ -0,0 +1,34 @@
+class SrmStudent {
+
+    static String collegeName;
+    static String academicYear;
+
+    String studentName;
+
+    static {
+        collegeName = "SRM Institute of Science and Technology";
+        academicYear = "2026-2027";
+        System.out.println("College info loaded");
+    }
+
+    public SrmStudent(String studentName) {
+        this.studentName = studentName;
+    }
+}
+
+public class SrmStudentDemo {
+
+    public static void main(String[] args) {
+
+        String[] names = {
+            "Ravi", "Meera", "Karthik", "Divya", "Anitha"
+        };
+
+        for (String name : names) {
+            SrmStudent student = new SrmStudent(name);
+            System.out.println(
+                "Student record created: " + student.studentName
+            );
+        }
+    }
+}
