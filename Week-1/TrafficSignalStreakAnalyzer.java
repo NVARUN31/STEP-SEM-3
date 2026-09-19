0cf537d0f45348fff7c8a8d6e9f60113902da90a
NVARUN31 <vn6756@srmist.edu.in>
2026-09-19 18:10:48 +0530

    Add Week 1 homework assignments

---
+++ Week-1/TrafficSignalStreakAnalyzer.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class TrafficSignalStreakAnalyzer {
+
+    static void findLongestStreak(String signalLog) {
+
+        char longestColor = signalLog.charAt(0);
+        int longestStreak = 1;
+
+        char currentColor = signalLog.charAt(0);
+        int currentStreak = 1;
+
+        for (int i = 1; i < signalLog.length(); i++) {
+
+            if (signalLog.charAt(i) == currentColor) {
+                currentStreak++;
+            } else {
+                currentColor = signalLog.charAt(i);
+                currentStreak = 1;
+            }
+
+            if (currentStreak > longestStreak) {
+                longestStreak = currentStreak;
+                longestColor = currentColor;
+            }
+        }
+
+        System.out.println("\n===== TRAFFIC SIGNAL STREAK ANALYZER =====");
+        System.out.println("Longest Streak: '" + longestColor +
+                "' repeated " + longestStreak + " times");
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter signal log (R/Y/G): ");
+        String signalLog = scanner.nextLine().toUpperCase();
+
+        findLongestStreak(signalLog);
+
+        scanner.close();
+    }
+}
